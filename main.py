bf1d0e76819cae3739a39ef5dd2682f7ef0d9224
Max <maxmoebius@hotmail.de>
2022-12-01 11:02:26 +0100

    Added code for feature matching and KI image generation.

--- main.py
+++
@@ -1,8 +0,0 @@
-import pyterrier as pt
-
-if __name__ == '__main__':
-    pt.init()
-    dataset = pt.get_dataset('irds:touche-image/2022-06-13')
-    # Index touche-image/2022-06-13
-    indexer = pt.IterDictIndexer('./indices/touche-image_2022-06-13')
-    index_ref = indexer.index(dataset.get_corpus_iter(), fields=['url', 'phash'])
