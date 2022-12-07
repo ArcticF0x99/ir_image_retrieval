ac46015d314a38556e354f88313a9403de4ebec8
Max <maxmoebius@hotmail.de>
2022-12-07 20:55:03 +0100

    Changed docker run command for using image of task 3 and make gpu use possible. Some more changes for feature matching.

--- container/start-jupyter-docker.sh
+++ container/start-jupyter-docker.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-docker run --rm -ti \
+docker run --rm -ti --gpus=all \
     -p 8888:8888 \
     -v ${PWD}/touche-code/clef23/image-retrieval-for-arguments/baseline-pyterrier:/workspace/baseline \
     -v ${PWD}/dataset22:/workspace/dataset22 \
     -v ${PWD}/workspace:/workspace \
-    webis/tira-touche23-task-2-pyterrier-baseline:0.0.1 \
+    webis/tira-touche23-task-3-pyterrier-baseline:0.0.1 \
     jupyter-lab --allow-root --ip 0.0.0.0
+
